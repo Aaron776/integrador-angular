86ee07d7228d8ff9dc3bdd80f4317caa2479c8b1
Aaron Ortiz <aronortiz90@yahoo.com>
2021-01-11 09:05:54 -0500

    crear pantalla error 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './login/login.component';
+import {ErrorNotFoundComponent} from './error-not-found/error-not-found.component';
+
 
 const routes: Routes = [
-  {path:'',component:LoginComponent}
+  {path:'',component:LoginComponent},
+  {path: '**', pathMatch: 'full', component:ErrorNotFoundComponent}
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
