8dd53b96822ca7be97407af4cd1f953ecd4a4414
ecaiza0015 <ecaiza0015@gmail.com>
2021-01-18 09:33:15 -0500

    View and create activity

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {ErrorNotFoundComponent} from './error-not-found/error-not-found.component';
-
+import { ViewActivityComponent } from './view-activity/view-activity.component';
+import {CreateActivityComponent} from './create-activity/create-activity.component';
 
 const routes: Routes = [
   {path:'',component:LoginComponent},
+  {path:'activity/:id',component:ViewActivityComponent},
+  {path:'createactivity',component:CreateActivityComponent},
   {path: '**', pathMatch: 'full', component:ErrorNotFoundComponent}
+  
 ];
 
 
