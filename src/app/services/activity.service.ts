36d328245a64e6bbc1748041706c496e2174c4c9
Aaron Ortiz <aronortiz90@yahoo.com>
2021-01-20 13:54:19 -0500

    cambios

--- src/app/services/activity.service.ts
+++ src/app/services/activity.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { ActivityModel } from '../models/activity.model';
 @Injectable({
   providedIn: 'root'
 })
 export class ActivityService {
+  private api = 'http://localhost:8080/api/v1/activities';
+  constructor(private http: HttpClient) {
+  }
+  getAllActivityByEmployee(id: number): Observable<any> {
+    return this.http.get(this.api + '/employee/' + id).pipe(
+      map(response => response),
+      catchError(error => {
+        alert(error.error);
+        return error;
+      })
+    );
+  }
 
-  constructor() { }
+  createActivity(activity: ActivityModel): Observable<any> {
+    return this.http.post(this.api, activity).pipe(
+      map(response => response),
+      catchError(error => {
+        alert(error.error);
+        return error;
+      })
+    );
+  }
+
 }
+
+
